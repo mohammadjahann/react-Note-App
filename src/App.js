c1e55f69cce012975715179eb71f3cf096e94efc
Jahan <you@example.com>
2025-08-02 17:33:55 +0330

    Initial project setup and NoteApp file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NoteApp from './components/NoteApp/NoteApp';
+
+function App() {
+  return (
+    <div>
+      <NoteApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NoteApp/NoteApp.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+export default class NoteApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Note App</h1>
+      </div>
+    );
+  }
+}
